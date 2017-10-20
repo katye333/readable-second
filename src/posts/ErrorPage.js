a7d1882885bb18acad07eabe0ce5bc06ef4e6a8b
Katye <katye333@gmail.com>
2017-10-20 11:15:30 -0400

    fix: Style Error Page

--- src/posts/ErrorPage.js
+++ src/posts/ErrorPage.js
@@ -4,13 +4,20 @@ import { Link } from 'react-router-dom';
 class ErrorPage extends Component {
 	render() {
 		return (
-			<div className="w3-container post_container" style={{ marginLeft: '250px', marginTop: '20px', width: '50%', marginBottom: '100px' }}>
-				<div className="w3-panel w3-red">
-				    <h3>Error 404</h3>
-				    <p>The page you were looking for is no longer available.</p>
+			<div className="w3-container post_container" style={{ marginTop: '20px', width: '60%', marginBottom: '100px' }}>
+				<div className="w3-card-4">
+					<div className="w3-container w3-red">
+				    	<h3>Error 404</h3>
+				    </div>
 
-				    <Link to={'/'} className="w3-btn w3-blue">Home</Link>
-				  </div>
+				    <div className="w3-container">
+				    	<p>The page you were looking for is no longer available.</p>
+				    </div>
+
+				    <div className="w3-container w3-red">
+				    	<Link to={'/'} className="w3-button">Home</Link>
+				    </div>
+				</div>
 			</div>
 		)
 	}
